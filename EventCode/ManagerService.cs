70183cd0860727106a8404fa5e3c7f57fb06391d
Wannes Gennar <wannes.gennar@gmail.com>
2021-10-14 21:27:32 +0200

    Added event (handler) examples

---
+++ EventCode/ManagerService.cs
@@ -0,0 +1,14 @@
+﻿namespace EventCode
+{
+    public class ManagerService
+    {
+        public delegate void SomethingHandler(string eventThatHappened);
+        public event SomethingHandler Something;
+
+        public void DoSomething()
+        {
+            // This triggers the handlers of the "Something" event
+            Something("Something has happened!");
+        }
+    }
+}
---
+++ EventCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using EventCode;
+
+void Handler1(string name)
+{
+    Console.WriteLine("Handler 1");
+}
+
+void Handler2(string name)
+{
+    Console.WriteLine("Handler 2");
+}
+
+var manager = new ManagerService();
+manager.Something += Handler1; // Subscribes Handler1 to the Something event
+manager.Something += Handler2; // Subscribes Handler2 to the Something event
+
+manager.DoSomething();
+Console.WriteLine("--- second event trigger ---");
+
+manager.Something -= Handler1;
+
+manager.DoSomething();
