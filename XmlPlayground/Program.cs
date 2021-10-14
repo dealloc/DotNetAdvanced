c3a9b7fef42b6519c9e6e1345534ee2371d11796
Wannes Gennar <wannes.gennar@gmail.com>
2021-10-14 18:56:26 +0200

    Added XML code examples

---
+++ XmlPlayground/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+using XmlPlayground;
+
+var serializer = new XmlSerializer(typeof(PurchaseOrder));
+var stream = new FileStream("purchaseorder.xml", FileMode.Open, FileAccess.Read);
+
+PurchaseOrder? purchaseorder = (PurchaseOrder?)serializer.Deserialize(stream);
+
+purchaseorder.PurchaseOrderNumber = "99504";
+
+var outputstream = new FileStream("result.xml", FileMode.CreateNew, FileAccess.Write);
+serializer.Serialize(outputstream, purchaseorder);
+
+return;
---
+++ XmlPlayground/PurchaseOrder.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlPlayground
+{
+    [XmlRoot("purchase-order")]
+    public class PurchaseOrder
+    {
+        [XmlAttribute("PurchaseOrderNumber")]
+        public string PurchaseOrderNumber { get; set; }
+        [XmlElement("Address")]
+        public Address Address { get; set; }
+        [XmlArray("Items"), XmlArrayItem("Item")]
+        public List<Item> Items { get; set; }
+    }
+
+    public class Address
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Street")]
+        public string Street { get; set; }
+        [XmlElement("City")]
+        public string City { get; set; }
+        [XmlElement("State")]
+        public string State { get; set; }
+        [XmlElement("Zip")]
+        public string Zip { get; set; }
+        [XmlElement("Country")]
+        public string Country { get; set; }
+    }
+
+    public class Item
+    {
+        [XmlAttribute("PartNumber")]
+        public string PartNumber { get; set; }
+
+        [XmlElement("ProductName")]
+        public string ProductName { get; set; }
+    }
+}
